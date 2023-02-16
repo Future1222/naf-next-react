fc1990b88dae0d217cbbed767317e8abdae7ccba
ducphamM2VN <duc.pm@mmh.co.jp>
2023-02-16 13:12:37 +0700

    update services of user

--- src/modules/services/user.services.ts
+++ src/modules/services/user.services.ts
@@ -2,19 +2,5 @@ import { IUser } from '@/modules/models/user.model'
 import { BaseRepository } from '@/shared/database/base.repository'
 
 export default class UserServices extends BaseRepository<IUser> {
-  findById(id: string): Promise<IUser> {
-    return this.findById(id)
-  }
-  findAll(): Promise<IUser[]> {
-    return this.findAll()
-  }
-  create(item: IUser): Promise<boolean> {
-    return this.create(item)
-  }
-  update(id: string, item: IUser): Promise<boolean> {
-    return this.update(id, item)
-  }
-  delete(id: string): Promise<boolean> {
-    return this.delete(id)
-  }
+  // All of the necessary function is imported
 }
