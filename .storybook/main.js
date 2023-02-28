223a67b0e75eef9ccc40af7e37408150fc55c8f4
lequan <jfuture222@gmail.com>
2023-02-28 20:33:13 +0700

    (chore) remove CaseSensitivePathsPlugin in the storybook config file

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,30 +1,33 @@
 module.exports = {
-  stories: [
-    '../src/components/**/*.stories.mdx',
-    '../src/components/**/*.stories.@(js|jsx|ts|tsx)',
-  ],
-  addons: [
-    '@storybook/addon-links',
-    '@storybook/addon-essentials',
-    '@storybook/addon-interactions',
-  ],
-  framework: '@storybook/react',
-  core: {
-    builder: '@storybook/builder-webpack5',
-  },
-  typescript: {
-    check: false,
-    checkOptions: {},
-    reactDocgen: false,
-    reactDocgenTypescriptOptions: {
-      shouldExtractLiteralValuesFromEnum: true,
-      propFilter: (prop) => (prop.parent ? !/node_modules/.test(prop.parent.fileName) : true),
+    stories: [
+        '../src/components/**/*.stories.mdx',
+        '../src/components/**/*.stories.@(js|jsx|ts|tsx)',
+    ],
+    addons: [
+        '@storybook/addon-links',
+        '@storybook/addon-essentials',
+        '@storybook/addon-interactions',
+    ],
+    framework: '@storybook/react',
+    core: {
+        builder: '@storybook/builder-webpack5',
     },
-  },
-  webpackFinal: async (config) => {
-    config.resolve.fallback = {
-      fs: false,
-    }
-    return config
-  },
+    typescript: {
+        check: false,
+        checkOptions: {},
+        reactDocgen: false,
+        reactDocgenTypescriptOptions: {
+            shouldExtractLiteralValuesFromEnum: true,
+            propFilter: (prop) => (prop.parent ? !/node_modules/.test(prop.parent.fileName) : true),
+        },
+    },
+    webpackFinal: async(config) => {
+        config.resolve.fallback = {
+            fs: false,
+        }
+        config.plugins = config.plugins.filter(
+            (plugin) => plugin.constructor.name !== 'CaseSensitivePathsPlugin'
+        )
+        return config
+    },
 }
